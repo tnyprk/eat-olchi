095b6cb39b699f6d38e612d23154ae9b43fbc409
tnyprk <tonysj.park97@gmail.com>
2026-03-08 22:56:20 -0400

    Add beef kalbi and update store hours

---
+++ src/data/storeData.js
@@ -0,0 +1,47 @@
+export const storeData = {
+  name: "Olchi",
+  phone: "5102835007",
+  email: "info@eat-olchi.com",
+
+  address: { street: "4068 San Pablo Dam Rd", city: "El Sobrante", state: "CA", zip: "94803" },
+
+  hours: {
+    monday: {
+      open: "11:30",
+      close: "21:00",
+      break: { start: "15:00", end: "16:30" }
+    },
+    tuesday: {
+      open: "11:30",
+      close: "21:00",
+      break: { start: "15:00", end: "16:30" }
+    },
+    wednesday: {
+      open: "11:30",
+      close: "21:00",
+      break: { start: "15:00", end: "16:30" }
+    },
+    thursday: {
+      open: "11:30",
+      close: "21:00",
+      break: { start: "15:00", end: "16:30" }
+    },
+    friday: { open: "11:30", close: "21:00" },
+    saturday: { open: "11:30", close: "21:00" },
+    sunday: null
+  },
+
+  social: { instagram: "eat_olchi" },
+
+  website: "eat-olchi.com",
+
+  delivery: {
+    doordash: "https://www.doordash.com/store/olchi-el-sobrante-38482084/87548586/",
+    grubhub:
+      "https://grubhub.com/restaurant/olchi-4068-san-pablo-dam-rd-el-sobrante/13406424?classicAffiliateId=%2Fr%2Fw%2F13406424%2F&utm_source=restaurant.grubhub.com&utm_medium=OOL&utm_campaign=order%20online&utm_content=13406424"
+  },
+
+  announcement: "We're OPEN! Visit us for our grand opening specials",
+
+  established: 2024
+};
